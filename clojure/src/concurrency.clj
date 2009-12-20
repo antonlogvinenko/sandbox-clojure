06b0b630a08a5a8b811551ef81e7d3a2a2288e80
blogytalky <anton.logvinenko@gmail.com>
2009-12-20 20:20:02 +1000

    Added some dummy clojure code.

--- clojure/src/concurrency.clj
+++ clojure/src/concurrency.clj
@@ -1,2 +1,62 @@
 (ns concurrency (:use clojure.contrib.test-is))
 
+;refs and transactional memory
+(def current-track (ref "Mars"))
+
+(is (= (deref current-track) "Mars"))
+(is (= @current-track "Mars"))
+
+(dosync (ref-set current-track "Venus"))
+(is (= @current-track "Venus"))
+
+(def messages (ref ()))
+(defn add-message [msg]
+  (dosync (alter messages conj msg)))
+(add-message "abc")
+(add-message "def")
+(is (= @messages ["def" "abc"]))
+
+(defn add-message [msg]
+  (dosync (commute messages conj msg)))
+(add-message "ghi")
+(add-message "jkl")
+
+(defstruct message :sender :text)
+(def validate-message-list
+  (partial every? #(and (:sender %) (:text %))))
+(def messages (ref () :validator validate-message-list))
+(add-message (struct message "stu" "legit message"))
+(is (= @messages '({:sender "stu" :text "legit message"})))
+
+;atoms
+(def current-track (atom {:title "Credo" :composer "Byrd"}))
+(is (= (deref current-track) {:title "Credo" :composer "Byrd"}))
+(is (= @current-track {:title "Credo" :composer "Byrd"}))
+
+(reset! current-track {:title "Spem in Alium" :composer "Tallis"})
+(is (= @current-track {:title "Spem in Alium" :composer "Tallis"}))
+
+(swap! current-track assoc :title "Sancte Deus")
+(is (= @current-track {:title "Sancte Deus" :composer "Tallis"}))
+
+
+;agents
+(def counter (agent 0))
+(send counter inc)
+;use sendoff for blocking function
+(is (= @counter 1))
+;(await & agents)
+(await-for 500 counter)
+
+(def counter (agent 0 :validator number?))
+(send counter (fn [] "boo"))
+;(println @agent now complains about exception: errors)
+;(println (agent-errors counter))
+(clear-agent-errors counter)
+(is (= @counter 0))
+
+
+;per-thread vars
+;def
+;binding
+;set! for thread local dynamic binding
